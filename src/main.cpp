7ba1ae46afed59d4647d76e74069d5e8bd9e45cf
Kai Kiehl <kai.kiehl@gmx.de>
2019-06-02 13:59:18 +0200

    a cube (with manual lines)

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,9 @@ void drawToConsole(screen *scrn)
 		for (uint y = 0; y < scrn->getYsize(); y++)
 		{
 			if (scrn->at(y,x))
-				std::cout << "X";
+				std::cout << "+";
 			else
-				std::cout << " ";
+				std::cout << "#";
 		}
 		std::cout << "\n";
 	}
@@ -21,14 +21,39 @@ void drawToConsole(screen *scrn)
 
 int main()
 {
-	Geometryf::Polytopef poly0 = Geometryf::Polytopef({ Geometryf::Pointf({0,0,0}),Geometryf::Pointf({10,0,0}),Geometryf::Pointf({10,10,0}),Geometryf::Pointf({10,10,10}),Geometryf::Pointf({0,10,0}),Geometryf::Pointf({0,10,10}),Geometryf::Pointf({0,0,10})});
-screen scrn = screen(20,20);
+
+
+screen scrn = screen(40,40);
 world w0 = world(&scrn);
+
+
+Geometryf::Polytopef poly0 = Geometryf::Polytopef({Geometryf::Pointf({5,0,0,0}),Geometryf::Pointf({15,0,0,0}),Geometryf::Pointf({5,10,0,0}),Geometryf::Pointf({5,10,-10,0}),Geometryf::Pointf({5,0,0,0}),Geometryf::Pointf({5,10,0,0}),Geometryf::Pointf({5,10,-10,0})});
+
+{
+	using namespace Geometryf;
+	Linef({Pointf({10,0,0,0}),Pointf({20,0,0,0})}).Draw(&scrn);
+	Linef({Pointf({20,0,0,0}),Pointf({20,10,0,0})}).Draw(&scrn);
+	Linef({Pointf({10,10,0,0}),Pointf({20,10,0,0})}).Draw(&scrn);
+	Linef({Pointf({10,0,0,0}),Pointf({10,10,0,0})}).Draw(&scrn);
+	Linef({Pointf({10,0,0,0}),Pointf({10,0,10,0})}).Draw(&scrn);
+	Linef({Pointf({20,0,0,0}),Pointf({20,0,10,0})}).Draw(&scrn);
+	Linef({Pointf({10,10,0,0}),Pointf({10,10,10,0})}).Draw(&scrn);
+	Linef({Pointf({20,10,0,0}),Pointf({20,10,10,0})}).Draw(&scrn);
+	Linef({Pointf({10,0,10,0}),Pointf({20,0,10,0})}).Draw(&scrn);
+	Linef({Pointf({10,0,10,0}),Pointf({10,10,10,0})}).Draw(&scrn);
+	Linef({Pointf({10,10,10,0}),Pointf({20,10,10,0})}).Draw(&scrn);
+	Linef({Pointf({20,0,10,0}),Pointf({20,10,10,0})}).Draw(&scrn);
+}
+
+
+
+
+
 w0.addDrawableObject(&poly0);
 
 Geometryf::Pointf p0 = Geometryf::Pointf({0,5,3});
 w0.addDrawableObject(&p0);
-poly0.Draw(&scrn);
+//poly0.Draw(&scrn);
 drawToConsole(&scrn);
 
 return 0;
